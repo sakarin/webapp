4e15161f5b51d13f06bf7a2b5288c23008d8a2fd
Sakarin Radchapunya <sakarin.my@gmail.com>
2012-03-28 20:19:18 +0700

    localtion-block finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,36 +3,39 @@ class ApplicationController < ActionController::Base
 
   def location_block
     #@geo = Geokit::Geocoders::MultiGeocoder.geocode("216.113.188.64")
+
+    # Geo Production
     @geo = Geokit::Geocoders::MultiGeocoder.geocode(request.ip)
+
+    # Thai
     #@geo = Geokit::Geocoders::MultiGeocoder.geocode("124.122.152.111")
 
     logger.debug request.ip
 
-    if "124.122.152.111" == request.ip
-      logger.debug "location0"
+    if "#{Spree::Config[:admin_ip]}" == request.ip
 
     elsif not Spree::Location.where(:country => @geo.country_code, :state => "", :zip => "", :city => "").blank?
       @location = Spree::Location.where(:country => @geo.country_code, :state => "", :zip => "", :city => "")
-      logger.debug "location1"
+
       redirect_to "#{@location.last.code}"
 
     elsif not Spree::Location.where(:country => @geo.country_code, :state => @geo.state, :zip => "", :city =>"").blank?
       @location = Spree::Location.where(:country => @geo.country_code, :state => @geo.state, :zip => "", :city =>"")
-      logger.debug "location2"
+
       redirect_to "#{@location.last.code}"
 
     elsif not Spree::Location.where(:country => @geo.country_code, :state => @geo.state, :zip => @geo.zip, :city => "").blank?
       @location = Spree::Location.where(:country => @geo.country_code, :state => @geo.state, :zip => @geo.zip, :city => "")
-          logger.debug "location3"
+
       redirect_to "#{@location.last.code}"
 
     elsif not Spree::Location.where(:country => @geo.country_code, :state => @geo.state, :zip => @geo.zip, :city => @geo.city).blank?
       @location = Spree::Location.where(:country => @geo.country_code, :state => @geo.state, :zip => @geo.zip, :city => @geo.city)
-          logger.debug "location4"
+
       redirect_to "#{@location.last.code}"
 
     else
-      logger.debug "NONE US"
+
 
     end
 
